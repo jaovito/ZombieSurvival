c9a827f1096f6cabaaea7470f081d81ee2fcf1da
jaovito <junior.vitorio.dias@gmail.com>
2024-11-01 21:55:52 -0300

    FEAT: Add shoot interface to communicate with classes and blueprints

---
+++ Source/ZombieSurvival/ShooterPlayer/Interfaces/ShooterCharacterInterface.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "ShooterCharacterInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UShooterCharacterInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IShooterCharacterInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Shooter Character Interface")
+	void ResetShooting();
+};
