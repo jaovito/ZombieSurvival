afccc7405410a16afcbea92ecf627de8fe372a9d
jaovito <junior.vitorio.dias@gmail.com>
2024-11-08 08:53:32 -0300

    FEAT: Add character status component

---
+++ Source/ZombieSurvival/CharacterStatusComponent.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterStatusComponent.h"
+
+
+UCharacterStatusComponent::UCharacterStatusComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UCharacterStatusComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UCharacterStatusComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                        FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+void UCharacterStatusComponent::TakeDamage(float Damage)
+{
+	Health -= Damage;
+	if (Health <= 0.0f)
+	{
+		Health = 0.0f;
+		// Call a function to handle death
+	}
+}
+
+void UCharacterStatusComponent::Heal(float Amount)
+{
+	Health += Amount;
+	if (Health > 100.0f)
+	{
+		Health = 100.0f;
+	}
+}
+
+void UCharacterStatusComponent::RestoreStamina(float Amount)
+{
+	Stamina += Amount;
+	if (Stamina > 100.0f)
+	{
+		Stamina = 100.0f;
+	}
+}
+
+void UCharacterStatusComponent::DrainStamina(float Amount)
+{
+	Stamina -= Amount;
+	if (Stamina < 0.0f)
+	{
+		Stamina = 0.0f;
+	}
+}
+
+float UCharacterStatusComponent::GetHealth()
+{
+	return Health;
+}
+
+float UCharacterStatusComponent::GetStamina()
+{
+	return Stamina;
+}
---
+++ Source/ZombieSurvival/CharacterStatusComponent.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CharacterStatusComponent.generated.h"
+
+
+UCLASS(ClassGroup=(CharacterComponent), meta=(BlueprintSpawnableComponent))
+class ZOMBIESURVIVAL_API UCharacterStatusComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UCharacterStatusComponent();
+	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	float Health = 100.0f;
+	float Stamina = 100.0f;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float Damage);
+	
+	UFUNCTION(BlueprintCallable)
+	void Heal(float Amount);
+
+	UFUNCTION(BlueprintCallable)
+	void RestoreStamina(float Amount);
+
+	UFUNCTION(BlueprintCallable)
+	void DrainStamina(float Amount);
+
+
+	UFUNCTION(BlueprintPure)
+	float GetHealth();
+
+	UFUNCTION(BlueprintPure)
+	float GetStamina();
+};
